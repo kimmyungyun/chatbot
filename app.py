fa61c6d17df30f62299db367f5c86231ce10b0f9
myungyun <100angru@gmail.com>
2019-12-13 11:10:44 +0900

    web에서 정보 받아서 메시지 보내기

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-import test # API Key Save
-from flask import Flask, escape, request
-
+from flask import Flask, escape, request, render_template
+from decouple import config
+import requests
+token = config('TELEGRAM_BOT_TOEKN')
+url = f'https://api.telegram.org/bot{token}/'
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get('name', "World")
     return f"Hello, {escape(name)}!"
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+@app.route('/send')
+def send():
+    request.args.get('user_id')
+    res = requests.get(url+'getUpdates').json()
+
+    user_id = res['result'][0]['message']['from']['id']
+    send_text = request.args.get('user_text')
+    send_url = f'{url}sendMessage?chat_id={user_id}&text={send_text}'
+
+    result = requests.get(send_url)
+
+    return render_template('send.html')
 
 if __name__=='__main__':
     app.run(debug=True)
